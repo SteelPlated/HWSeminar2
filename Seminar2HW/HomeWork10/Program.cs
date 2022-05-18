a5af3f40de064d2d798b31ed2f31f373b5410c72
SteelPlated <mix.singatulin@yandex.ru>
2022-05-18 23:47:56 +0500

    HW10

---
+++ Seminar2HW/HomeWork10/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая принимает на
+// вход трёхзначное число и на выходе показывает вторую
+// цифру этого числа.
+
+Console.Write("Введите трехзначное число :");
+int a = int.Parse(Console.ReadLine());
+
+int first = a / 100;
+int last = a % 10;
+int res = (a - first * 100 - last) / 10;
+Console.WriteLine($"Результат = {res}");
