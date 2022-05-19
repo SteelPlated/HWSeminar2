8c0e45ab0972070675f08500a6df38a8ee0747e4
SteelPlated <mix.singatulin@yandex.ru>
2022-05-19 10:36:07 +0500

    HW10

--- Seminar2HW/HomeWork10/Program.cs
+++ Seminar2HW/HomeWork10/Program.cs
@@ -2,11 +2,14 @@
 // вход трёхзначное число и на выходе показывает вторую
 // цифру этого числа.
 
-Console.Write ("Введите трехзначное число :");
+Console.Write("Введите трехзначное число :");
 int a = int.Parse(Console.ReadLine());
-if ((a<1000)&&(a>99))
-    {int first = a/100;
-    int last = a%10;
-    int res =(a - first * 100 - last)/10;
-    Console.WriteLine($"Результат = {res}");}
-else Console.WriteLine("Ваше число не подходит. Нужно ТРЕХЗАЧНОЕ!!!");
+if ((a < 1000) && (a > 99))
+{
+    int first = a / 100;
+    int last = a % 10;
+    int res = (a - first * 100 - last) / 10;
+    Console.WriteLine($"Результат = {res}");
+}
+else
+    Console.WriteLine("Ваше число не подходит. Нужно ТРЕХЗАЧНОЕ!!!");
