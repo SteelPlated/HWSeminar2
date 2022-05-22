75c2a8724c9b0a0d0fb81409afe5b5d516d6c2ed
SteelPlated <mix.singatulin@yandex.ru>
2022-05-22 10:49:06 +0500

    для себя

--- Seminar2HW/Homework15/Program.cs
+++ Seminar2HW/Homework15/Program.cs
@@ -2,17 +2,26 @@
 // вход цифру, обозначающую день недели, и проверяет,
 // является ли этот день выходным.
 
-Console.Write("Введите номер дня недели: ");
+/*Console.Write("Введите номер дня недели: ");
 int Number = Convert.ToInt32(Console.ReadLine());
 
-    if (Number == 6 || Number == 7)
-    {
-        Console.WriteLine("(Выходной) -> да");
-    }
-    else
-    if (Number < 1 || Number > 7)
-    {
-        Console.WriteLine("Такого дня недели не существует");
-    }
-    else 
-        Console.WriteLine("(Не является выходным) -> нет");
+if (Number == 6 || Number == 7)
+{
+    Console.WriteLine("(Выходной) -> да");
+}
+else
+if (Number < 1 || Number > 7)
+{
+    Console.WriteLine("Такого дня недели не существует");
+}
+else
+    Console.WriteLine("(Не является выходным) -> нет");*/
+
+Console.Write(" Введите число дня недели 1 до 7 : ");
+var day = Convert.ToInt32(Console.ReadLine());
+string[] isWeekend = { " no ", " no ", " no ", " no ", " no ", " y ", " y " };
+if (day > 0 && day < 8)
+{
+    Console.WriteLine(isWeekend[day - 1]);
+}
+else Console.WriteLine(" Вы ввели некорректиое значение ! ");
